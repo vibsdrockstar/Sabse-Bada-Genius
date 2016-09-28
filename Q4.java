990eebe651465f8de060a8014eb47200c80d0dca
Vibhor <bijoy.vibhor19@gmail.com>
2016-09-28 20:25:18 +0530

    Create Q4.java

---
+++ Q4.java
@@ -0,0 +1,35 @@
+package com.example.vibhor.sabsebadagenius;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Q4 extends AppCompatActivity {
+    EditText stuans;
+    Button b_nq5 ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_q4);
+    }
+
+    public void checkanswer(View v)
+    {
+        EditText stuans = (EditText) findViewById(R.id.stuans);
+        if(stuans.getText().toString().toLowerCase().equals("leonardo di caprio"))
+        {
+            Toast.makeText(Q4.this, "Correct answer +5", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(Q4.this, "Incorrect answer O points", Toast.LENGTH_SHORT).show();
+        }
+        Intent nextPage = new Intent(Q4.this,Q5.class);
+        startActivity(nextPage);
+    }
+}
+
